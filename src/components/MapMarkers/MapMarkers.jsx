d59c15ce3375147d518eb532a3cc8af159acef9b
adamascencio <ascencioadam@gmail.com>
2022-12-21 13:46:33 -0800

    add location marker icons which show details of each location on hover

--- src/components/MapMarkers/MapMarkers.jsx
+++ src/components/MapMarkers/MapMarkers.jsx
@@ -1,29 +1,34 @@
+import { useState } from 'react';
 import { Paper, Typography, Rating } from '@mui/material';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { StyledEngineProvider } from '@mui/material';
+import './MapMarkers.css';
 
 export function MapMarkers({ isMobile, place }) {
-  const Marker = isMobile ? (
-    <FontAwesomeIcon
-      icon={faLocationDot}
-      color='red'
-      size='xl'
-    />
-  ) : (
-    <Paper sx={{ ':hover': { boxShadow: 20 } }}> 
-      <img 
-        src={
-          place.photo ? 
-            place.photo.images.large.url 
-            : 
-            'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
-        }
-        alt={place.name} 
-      />
-      <Typography align='center' variant='subtitle2'>{place.name}</Typography>
-      <Rating value={parseFloat(place.rating)} readOnly />
-    </Paper>
-  );
+  const [showCard, setShowCard] = useState(false);
 
-  return Marker;
+  function showLocationCard() {
+    return (
+      <Paper sx={{ ':hover': { boxShadow: 20 } }} className='map-card'>
+        <img src={place.photo.images.small.url} alt={place.name} />
+        <Typography align='left' variant='subtitle2'>{place.name}</Typography>
+        <Rating value={parseFloat(place.rating)} readOnly />
+      </Paper>
+    );
+  }
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <FontAwesomeIcon 
+        icon={faLocationDot} 
+        color='red' 
+        size='2x' 
+        onMouseEnter={() => setShowCard(true)}
+        onMouseLeave={() => setShowCard(false)}
+        className='map-marker'
+      />
+      {showCard && showLocationCard()}
+    </StyledEngineProvider>
+  );
 }

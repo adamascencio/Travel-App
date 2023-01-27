b51389fa8ea447794122a625a6aaa7db4bf594ca
adamascencio <ascencioadam@gmail.com>
2023-01-26 17:11:05 -0800

    Add updated travel advisor api call as previous version has been depracated

--- src/api/places-service.js
+++ src/api/places-service.js
@@ -10,9 +10,36 @@ export async function getPlacesData(type, sw, ne) {
         tr_longitude: ne.lng
       }, 
       headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      }
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
   }
+}
+
+export async function updatedGetPlacesData(type, sw, ne) {
+  try {
+    const { data: { data } } = await axios.post(
+    `https://travel-advisor.p.rapidapi.com/${type}/v2/list`, {
+      params: {
+        boundingBox: {
+          northEastCorner: {
+            latitude: ne.lat,
+            longitude: ne.lng,
+          },
+          southWestCorner: {
+            latitude: sw.lat,
+            longitude: sw.lng,
+          }
+        }
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+      }
     });
     return data;
   } catch (error) {

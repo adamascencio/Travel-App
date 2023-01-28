2d9a22de20c362be981b89f30619c06723bce8f2
adamascencio <ascencioadam@gmail.com>
2023-01-28 14:26:51 -0800

    update v2 api call to make post request for locations

--- src/api/places-service.js
+++ src/api/places-service.js
@@ -21,28 +21,27 @@ export async function getPlacesData(type, sw, ne) {
 }
 
 export async function updatedGetPlacesData(type, sw, ne) {
-  try {
-    const { data: { data } } = await axios.post(
-    `https://travel-advisor.p.rapidapi.com/${type}/v2/list`, {
-      params: {
-        boundingBox: {
-          northEastCorner: {
-            latitude: ne.lat,
-            longitude: ne.lng,
-          },
-          southWestCorner: {
-            latitude: sw.lat,
-            longitude: sw.lng,
-          }
-        }
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-      }
-    });
-    return data;
-  } catch (error) {
-    console.log(error);
+  const options = {
+    method: 'POST',
+    url: `https://travel-advisor.p.rapidapi.com/${type}/v2/list`,
+    params: {currency: 'USD', units: 'mi', lang: 'en_US'},
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+    },
+    data: `{"geoId":293928,"sort":"POPULARITY","sortOrder":"desc","filters":[{"id":"establishment","value":["10591"]}],"boundingBox":{"northEastCorner":{"latitude":${ne.lat},"longitude":${ne.lng}},"southWestCorner":{"latitude":${sw.lat},"longitude":${sw.lng}}},"updateToken":""}`
   }
+
+  const res = axios.request(options).then(function (response) {
+    // Get array of places from response 
+    const places = response.data.data.AppPresentation_queryAppListV2[0].sections;
+    // filter location data from places
+    const locations = places.filter((place) => place.__typename === 'AppPresentation_SingleCard');
+    return locations;
+  }).catch(function (error) {
+    console.error('error: ', error);
+  });
+
+  return res;
 }

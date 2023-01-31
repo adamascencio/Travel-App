d849f1c94441851e68966c5585bae54075f3ca06
adamascencio <ascencioadam@gmail.com>
2023-01-31 15:26:47 -0800

    remove zIndex from map on mobile

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -14,8 +14,7 @@ export default function Map({ coordinates, setCoordinates, setBounds, places, se
       height: '100vh', 
       width: '100vw', 
       left: 0, 
-      bottom: 0,
-      zIndex: -100
+      bottom: 0
     } 
     : 
     { height: '89vh', width: '100%' };

920d67e749f41ef43749499f9612fe05924a9db6
adamascencio <ascencioadam@gmail.com>
2023-01-31 14:08:38 -0800

    remove v2 api post request

--- src/api/places-service.js
+++ src/api/places-service.js
@@ -19,26 +19,3 @@ export async function getPlacesData(type, sw, ne) {
     console.log(error);
   }
 }
-
-export async function updatedGetPlacesData(type, sw, ne) {
-  const options = {
-    method: 'POST',
-    url: `https://travel-advisor.p.rapidapi.com/${type}/v2/list`,
-    params: {currency: 'USD', units: 'mi', lang: 'en_US'},
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-    },
-    data: `{"geoId":293928,"sort":"POPULARITY","sortOrder":"desc","filters":[{"id":"establishment","value":["10591"]}],"boundingBox":{"northEastCorner":{"latitude":${ne.lat},"longitude":${ne.lng}},"southWestCorner":{"latitude":${sw.lat},"longitude":${sw.lng}}},"updateToken":""}`
-  }
-
-  const res = axios.request(options).then(function (response) {
-    const places = response.data.data.AppPresentation_queryAppListV2[0];
-    return places;
-  }).catch(function (error) {
-    console.error('error: ', error);
-  });
-
-  return res;
-}
